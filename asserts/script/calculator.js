02ee9b6de958c42afefa94e37d8561da93671c89
kadjialan <alankadji7@gmail.com>
2022-12-17 12:47:32 +0100

    linters resolution

--- asserts/script/calculator.js
+++ asserts/script/calculator.js
@@ -1,116 +1,115 @@
-const operators = document.querySelectorAll('.operation');
-const digits = document.querySelectorAll('.numb');
-const AC = document.getElementById('ac');
-const plusOrMinus = document.getElementById('plusminus');
-const percentage = document.getElementById('percent');
-const equals = document.getElementById('equals');
-const currentOperand = document.querySelector('#currentoperand span');
-const previousOperand = document.querySelector('#previousoperand span'); 
+const operators = document.querySelectorAll('.operation')
+const digits = document.querySelectorAll('.numb')
+const AC = document.getElementById('ac')
+const plusOrMinus = document.getElementById('plusminus')
+const percentage = document.getElementById('percent')
+const equals = document.getElementById('equals')
+const currentOperand = document.querySelector('#current_operand span')
+const previousOperand = document.querySelector('#previous_operand span')
 
+let currentOperation = ''
 
-let currentOperation = '';
-
-allClear();
+allClear()
 
 // Clear the calculator
-function allClear() {
-  currentOperand.textContent = '';
-  previousOperand.textContent = '';
-  currentOperation = '';
+function allClear () {
+  currentOperand.textContent = ''
+  previousOperand.textContent = ''
+  currentOperation = ''
 }
 
 // Append number to screen when number button pressed
- function appendNumber(number) {
-   if (number == '.' && currentOperand.textContent.includes('.')) {
-     return;
-   }
-   (currentOperand.textContent += number).toString;
-   if (
-     currentOperand.textContent[0] == '0' &&
-     currentOperand.textContent[1] == '0'   ) {
-     currentOperand.textContent = '0';
-   }
- }
+function appendNumber (number) {
+  if (number == '.' && currentOperand.textContent.includes('.')) {
+    return
+  }
+  (currentOperand.textContent += number).toString
+  if (
+    currentOperand.textContent[0] == '0' &&
+     currentOperand.textContent[1] == '0') {
+    currentOperand.textContent = '0'
+  }
+}
 
 // Update operation
-function updateOperation(operation) {
-  currentOperation = operation;
+function updateOperation (operation) {
+  currentOperation = operation
   if (currentOperand.textContent !== '' && previousOperand.textContent !== '') {
-    return;
+    return
   }
   if (currentOperand.textContent !== '') {
-    previousOperand.textContent = currentOperand.textContent;
-    currentOperand.textContent = '';
+    previousOperand.textContent = currentOperand.textContent
+    currentOperand.textContent = ''
   }
 }
 
 // Perform calculation and update display
-function calculate() {
-  let result = '';
-  let b = Number(currentOperand.textContent);
-  let a = Number(previousOperand.textContent);
-  console.log(a, b, currentOperation);
+function calculate () {
+  let result = ''
+  const b = Number(currentOperand.textContent)
+  const a = Number(previousOperand.textContent)
+  console.log(a, b, currentOperation)
 
   switch (currentOperation) {
     case 'plus':
-      result = a + b;
-      break;
+      result = a + b
+      break
     case 'minus':
-      result = a - b;
-      break;
+      result = a - b
+      break
     case 'times':
-      result = a * b;
-      break;
+      result = a * b
+      break
     case 'division':
-      result = a / b;
-      break;
+      result = a / b
+      break
     case 'percent':
-      result = a * (b / 100);
-      break;
+      result = a * (b / 100)
+      break
   }
-  currentOperand.textContent = result;
-  previousOperand.textContent = '';
-  currentOperation = 'equals';
+  currentOperand.textContent = result
+  previousOperand.textContent = ''
+  currentOperation = 'equals'
 }
 
 // Change the sign of currentOperand
-function changeSign() {
-  let b = Number(currentOperand.textContent);
+function changeSign () {
+  const b = Number(currentOperand.textContent)
   if (b >= 0) {
-    currentOperand.textContent = '-' + currentOperand.textContent;
+    currentOperand.textContent = '-' + currentOperand.textContent
   } else {
-    currentOperand.textContent = currentOperand.textContent.substring(1);
+    currentOperand.textContent = currentOperand.textContent.substring(1)
   }
 }
 
 // EVENT LISTENERS
-AC.addEventListener('click', allClear);
+AC.addEventListener('click', allClear)
 
-//number buttons
- digits.forEach((button) => {
-   button.addEventListener('click', () => {
-     if (currentOperation === 'equals') {
-      allClear();
-       appendNumber(button.textContent);
-     } else {
-       appendNumber(button.textContent);
-     }
-   });
- });
+// number buttons
+digits.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (currentOperation === 'equals') {
+      allClear()
+      appendNumber(button.textContent)
+    } else {
+      appendNumber(button.textContent)
+    }
+  })
+})
 
-//operation buttons
+// operation buttons
 operators.forEach((button) => {
   button.addEventListener('click', () => {
-    updateOperation(button.id);
-  });
-});
+    updateOperation(button.id)
+  })
+})
 
-//equals button
+// equals button
 equals.addEventListener('click', () => {
-  calculate();
-});
+  calculate()
+})
 
-//plusorMinus button
+// plusorMinus button
 plusOrMinus.addEventListener('click', () => {
-  changeSign();
-}); 
+  changeSign()
+})
